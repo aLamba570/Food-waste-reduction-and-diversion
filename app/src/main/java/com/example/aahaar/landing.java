da2743beea731c81feac029139faa8e20ccd4690
Ankush <alamba570@gmail.com>
2023-04-29 11:16:42 +0530

    Adds landing page

--- app/src/main/java/com/example/aahaar/landing.java
+++ app/src/main/java/com/example/aahaar/landing.java
@@ -1,15 +1,53 @@
 package com.example.aahaar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class landing extends AppCompatActivity {
 
+    CardView cardLogin,cardRegister,cardAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        cardAbout = findViewById(R.id.cardAboutus);
+        cardLogin = findViewById(R.id.cardLogin);
+        cardRegister = findViewById(R.id.cardRegister);
+
+
+        cardRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent k = new Intent(landing.this, SignUpActivity.class);
+                startActivity(k);
+                finish();
+            }
+        });
+
+        cardLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k = new Intent(landing.this, LoginActivity.class);
+                startActivity(k);
+                finish();
+            }
+        });
+
+
+        cardAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k = new Intent(landing.this, AboutActivity.class);
+                startActivity(k);
+                finish();
+            }
+        });
 
 
     }
