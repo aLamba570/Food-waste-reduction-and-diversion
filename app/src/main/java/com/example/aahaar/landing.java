bc0d448d663a1cdc3febf7ca412c8b4aae3fc417
Ankush <alamba570@gmail.com>
2023-04-29 11:03:29 +0530

    Adds landing page

---
+++ app/src/main/java/com/example/aahaar/landing.java
@@ -0,0 +1,16 @@
+package com.example.aahaar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+
+    }
+}
